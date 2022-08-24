33b4e32aa588c959c314f26685688fc689fe7a1c
95choiJH <80238745+95choiJH@users.noreply.github.com>
2022-08-24 15:50:14 +0900

    수정

--- asset/js/common.js
+++ asset/js/common.js
@@ -235,3 +235,23 @@ html += `</div>
 }).catch(function(error){
   console.error("에러");
 })
+$(function(){
+    
+  var swiper = new Swiper(".eventSlide", {
+    loopAdditionalSlides : 1,
+    loopedSlides: 2,
+    spaceBetween: 14,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  var swiper = new Swiper(".shopLive", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+})
